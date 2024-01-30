95272c75c43a92859d76a2f625e045ebc2f0cf0e
Oluwole Samson <oluwolefsamson44@gmail.com>
2024-01-30 10:22:30 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Hero from './Components/Hero/Hero';
+import About from './Components/About/About';
 
 function App() {
   return (
     <>
       < Navbar />
       < Hero />
+      < About />
     </>
   );
 }
